3b0f873be49d1280421783b7e58c23da6f775465
Niall Byrne <niall@niallbyrne.ca>
2021-03-09 18:02:24 +0000

    test(KITCHEN.MODELS.MANAGER.ITEM): refactor to improve speed

--- panic/kitchen/models/managers/item/tests/test_maintenance.py
+++ panic/kitchen/models/managers/item/tests/test_maintenance.py
@@ -7,7 +7,6 @@ from freezegun import freeze_time
 
 from .....exceptions import ConfirmationRequired
 from .....tests.fixtures.fixtures_transaction import TransactionTestHarness
-from ....inventory import Inventory
 from ....item import Item
 from ....transaction import Transaction
 
@@ -19,17 +18,13 @@ class TestMaintenanceManager(TransactionTestHarness):
   mute_signals = False
 
   @classmethod
-  def setUpTestData(cls):
-    super().setUpTestData()
+  def create_data_hook(cls):
     test_data = cls.create_dependencies(2)
     cls.user2 = test_data['user']
     cls.store2 = test_data['store']
     cls.shelf2 = test_data['shelf']
     cls.item2 = test_data['item']
-    cls.create_data_hook()
 
-  @classmethod
-  def create_data_hook(cls):
     cls.today = timezone.now()
 
     cls.one_year_ago = cls.today - timedelta(days=365)
@@ -40,17 +35,11 @@ class TestMaintenanceManager(TransactionTestHarness):
         timedelta(days=cls.item1.shelf_life, hours=10)
     )
 
-  def setUp(self):
-    super().setUp()
+    cls.purchases1 = cls._create_scenarios(30.1, cls.item1)
+    cls.consumptions1 = cls._create_scenarios(-1, cls.item1)
 
-    self.item2.quantity = 0
-    self.item2.save()
-
-    self.purchases1 = self._create_scenarios(30.1, self.item1)
-    self.consumptions1 = self._create_scenarios(-1, self.item1)
-
-    self.purchases2 = self._create_scenarios(60.1, self.item2)
-    self.consumptions2 = self._create_scenarios(-2, self.item2)
+    cls.purchases2 = cls._create_scenarios(60.1, cls.item2)
+    cls.consumptions2 = cls._create_scenarios(-2, cls.item2)
 
     activities = [
         'p-last_year',
@@ -61,40 +50,47 @@ class TestMaintenanceManager(TransactionTestHarness):
         'p-today',
     ]
 
-    self._generate_test_data(activities)
+    cls._generate_transactions_from_activities(activities)
+    cls._snapshot_item_quantities()
 
-  def tearDown(self):
-    super().tearDown()
-    Inventory.objects.all().delete()
-
-  def _generate_test_data(self, activities):
-    for activity in activities:
-      activity_type, when = activity.split('-')
-      if activity_type == 'p':
-        self._create_test_transaction(**self.purchases1[when])
-        self._create_test_transaction(**self.purchases2[when])
-      if activity_type == 'c':
-        self._create_test_transaction(**self.consumptions1[when])
-        self._create_test_transaction(**self.consumptions2[when])
-
-  def _create_scenarios(self, quantity, item):
+  @classmethod
+  def _create_scenarios(cls, quantity, item):
 
     def generate_transaction_data(date_object):
       return {'item': item, 'datetime': date_object, 'quantity': quantity}
 
     return {
-        'today': generate_transaction_data(self.today),
-        'last_week': generate_transaction_data(self.one_week_ago),
-        'last_month': generate_transaction_data(self.one_month_ago),
-        'last_year': generate_transaction_data(self.one_year_ago),
-        'timezone_edgecase': generate_transaction_data(self.timezone_edgecase),
+        'today': generate_transaction_data(cls.today),
+        'last_week': generate_transaction_data(cls.one_week_ago),
+        'last_month': generate_transaction_data(cls.one_month_ago),
+        'last_year': generate_transaction_data(cls.one_year_ago),
+        'timezone_edgecase': generate_transaction_data(cls.timezone_edgecase),
     }
 
-  def _create_test_transaction(self, **kwargs):
+  @classmethod
+  def _create_test_transaction(cls, **kwargs):
     transaction = Transaction(**kwargs)
     transaction.save()
-    self.objects.append(transaction)
     return transaction
+
+  @classmethod
+  def _generate_transactions_from_activities(cls, activities):
+    for activity in activities:
+      activity_type, when = activity.split('-')
+      if activity_type == 'p':
+        cls._create_test_transaction(**cls.purchases1[when])
+        cls._create_test_transaction(**cls.purchases2[when])
+      if activity_type == 'c':
+        cls._create_test_transaction(**cls.consumptions1[when])
+        cls._create_test_transaction(**cls.consumptions2[when])
+
+  @classmethod
+  def _snapshot_item_quantities(cls):
+    cls.original_item1_quantity = cls.item1.quantity
+    cls.original_item2_quantity = cls.item2.quantity
+
+  def setUp(self):
+    self.objects = list()
 
   def _refresh_items(self):
     self.item1.invalidate_caches()
@@ -116,15 +112,12 @@ class TestMaintenanceManager(TransactionTestHarness):
     Item.objects.rebuild_quantities_from_inventory(confirm=True)
 
   def test_rebuild_same_quantities(self):
-    item1_original_quantity = self.item1.quantity
-    item2_original_quantity = self.item2.quantity
-
     test_quantities = 10
     self._set_quantities(test_quantities)
 
     Item.objects.rebuild_quantities_from_inventory(confirm=True)
     self._refresh_items()
 
-    self.assertEqual(self.item1.quantity, item1_original_quantity)
+    self.assertEqual(self.item1.quantity, self.original_item1_quantity)
 
-    self.assertEqual(self.item2.quantity, item2_original_quantity)
+    self.assertEqual(self.item2.quantity, self.original_item2_quantity)
