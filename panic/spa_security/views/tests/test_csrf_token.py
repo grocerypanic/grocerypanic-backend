c19af470e1d1defee08b232ab5292e5adf3f7c75
Niall Byrne <niall@niallbyrne.ca>
2021-03-18 22:09:13 +0000

    test(SPA_SECURITY): move boundary on csrf token view tests

--- panic/spa_security/views/tests/test_csrf_token.py
+++ panic/spa_security/views/tests/test_csrf_token.py
@@ -9,14 +9,14 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
+from .. import csrf_token as view_module
+
 CSRF_URL = reverse("spa_security:csrf")
+VIEW_MODULE = view_module.__name__
 
 
 class PublicCSRFTest(TestCase):
   """Test the public CSRF API."""
-
-  def setUp(self):
-    self.client = APIClient()
 
   def test_login_required(self):
     resp = self.client.get(CSRF_URL)
@@ -35,25 +35,30 @@ class PublicCSRFTest(TestCase):
 class PrivateCSRFTest(TestCase):
   """Test the private CSRF API."""
 
-  def setUp(self):
-    self.user = get_user_model().objects.create_user(
+  @classmethod
+  def setUpTestData(cls):
+    cls.user = get_user_model().objects.create_user(
         username="testuser",
         email="test@niallbyrne.ca",
         password="test123",
     )
+
+  def setUp(self):
     self.client = APIClient(enforce_csrf_checks=True)
     self.client.force_authenticate(self.user)
 
   @patch(
-      "django.middleware.csrf._salt_cipher_secret",
+      VIEW_MODULE + '.get_token',
       new_callable=Mock(return_value=lambda x: "MockToken")
   )
-  def test_retrieve_message(self, _):
+  def test_token_is_set(self, _):
     resp = self.client.get(CSRF_URL)
 
     self.assertEqual(resp.status_code, status.HTTP_200_OK)
     self.assertEqual(resp.data["token"], "MockToken")
-    self.assertIn(settings.CSRF_COOKIE_NAME, resp.cookies)
 
-  def tearDown(self):
-    self.user.delete()
+  def test_cookie_is_set(self):
+    resp = self.client.get(CSRF_URL)
+
+    self.assertEqual(resp.status_code, status.HTTP_200_OK)
+    self.assertIn(settings.CSRF_COOKIE_NAME, resp.cookies)
