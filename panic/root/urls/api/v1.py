051fae27d42f51e1907193dd2e0615ad0952d549
Niall Byrne <niall@niallbyrne.ca>
2021-04-16 19:25:38 +0000

    feat(ROOT.URLS): seperate the user_details view into it's own endpoint

--- panic/root/urls/api/v1.py
+++ panic/root/urls/api/v1.py
@@ -1,8 +1,12 @@
 """URLS for the Panic v1 API."""
 
 from dj_rest_auth.registration.views import ConfirmEmailView, VerifyEmailView
-from dj_rest_auth.views import PasswordResetConfirmView
+from dj_rest_auth.views import PasswordResetConfirmView, UserDetailsView
 from django.urls import include, path
+
+from utilities.urls.include import selective_include
+
+UserDetailsView.__doc__ = "User Details API view."
 
 urlpatterns = [
     path(
@@ -23,7 +27,7 @@ urlpatterns = [
     ),
     path(
         "api/v1/auth/",
-        include('dj_rest_auth.urls'),
+        selective_include("dj_rest_auth.urls", blacklist="rest_user_details"),
     ),
     path(
         'api/v1/auth/registration/account-confirm-email/<str:key>/',
@@ -41,5 +45,10 @@ urlpatterns = [
     path(
         "api/v1/auth/social/",
         include("social_accounts.urls"),
+    ),
+    path(
+        "api/v1/users/",
+        UserDetailsView.as_view(),
+        name='rest_user_details',
     ),
 ]
